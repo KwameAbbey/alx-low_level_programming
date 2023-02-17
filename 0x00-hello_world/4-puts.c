7130dfe05bf704ce10b7770a9782cdd967b7a362
KwameAbbey <kwameabbey7@gmail.com>
2023-02-16 20:20:34 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* main - A program that print a line using the puts function
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+	return(0);
+}
